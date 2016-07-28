045d82fe3cc9cd7761263396fc369ea2c125f940
Martin <nuocmambi@gmail.com>
2016-07-28 18:55:00 +0200

    main activity lagt til

--- app/src/main/java/com/example/friends/fitfriend/MainActivity.java
+++ app/src/main/java/com/example/friends/fitfriend/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.friends.fitfriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String a = "DD";
-        String b = "yoloooooo";
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
