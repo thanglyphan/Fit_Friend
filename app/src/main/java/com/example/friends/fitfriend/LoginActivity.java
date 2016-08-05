fd5c44824b95ba6348b25dc64fcfc6d254a35ae9
X1337 <xohnny1337@gmail.com>
2016-08-05 16:48:16 +0200

    yolo

--- app/src/main/java/com/example/friends/fitfriend/LoginActivity.java
+++ app/src/main/java/com/example/friends/fitfriend/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.friends.fitfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String a = "DD";
-        String b = "yoloooooo";
         setContentView(R.layout.activity_login);
+    }
+    public void loginButton(View v) {
+        //if login successful go to activity main
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        super.onDestroy();
     }
 }
