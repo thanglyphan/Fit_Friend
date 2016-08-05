6c774a52affe672b8b2fc7b6dfe9781076ad80a5
X1337 <xohnny1337@gmail.com>
2016-08-05 18:29:01 +0200

    startActivity(intent);

--- app/src/main/java/com/example/friends/fitfriend/MainActivity.java
+++ app/src/main/java/com/example/friends/fitfriend/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.friends.fitfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void viewPButton(View v) {
+        Intent intent = new Intent(this, ViewprofileActivity.class);
+        startActivity(intent);
+
+    }
+    public void fListButton(View v) {
+        Intent intent = new Intent(this, FriendlistActivity.class);
+        startActivity(intent);
+
+    }
+    public void swipeButton(View v) {
+        Intent intent = new Intent(this, SwipeActivity.class);
+        startActivity(intent);
+
+    }
+
 }
--- app/src/main/java/com/example/friends/fitfriend/SwipeActivity.java
+++ app/src/main/java/com/example/friends/fitfriend/SwipeActivity.java
@@ -1,7 +1,9 @@
 package com.example.friends.fitfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SwipeActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SwipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
+    }
+    public void gotomain(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
